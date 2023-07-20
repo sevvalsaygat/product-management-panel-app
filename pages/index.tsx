b23eb27d084c01bcd51b4ee24316cdc0e3a1f955
sevvalsaygat <sevvallsstt@gmail.com>
2023-07-20 21:48:42 +0300

    feat: create product modal

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,28 @@
+import { useState } from 'react';
+
+import { ProductModal } from '@components';
+
 export default function Home() {
-  return <div>Home</div>;
+  let [isOpen, setIsOpen] = useState(false);
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  return (
+    <div>
+      <ProductModal isOpen={isOpen} closeModal={closeModal} />
+      <button
+        type="button"
+        onClick={openModal}
+        className="rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+      >
+        Create Product
+      </button>
+    </div>
+  );
 }
