817ed5c536282d027fdfcd4c80a0cb2cd3a270d3
sevvalsaygat <sevvallsstt@gmail.com>
2023-07-25 18:49:17 +0300

    feat: add tabs

--- components/ProductTable/index.tsx
+++ components/ProductTable/index.tsx
@@ -1,4 +1,8 @@
 /* eslint-disable @next/next/no-img-element */
+import { useState } from 'react';
+
+import { Tab } from '@headlessui/react';
+
 import { useProducts } from '@hooks';
 import { ClothesProductType, DrinkProductType, FoodProductType, ProductType } from '@/types';
 import ClothesItem from './ClothesItem';
@@ -12,43 +16,49 @@ type TablePropTypes = {
 const Table = ({}: TablePropTypes) => {
   const { products } = useProducts();
 
+  function classNames(...products: any[]) {
+    return products.filter(Boolean).join(' ');
+  }
+
+  const [categories] = useState({
+    Recent: [{}],
+    Popular: [{}],
+    Trending: [{}],
+  });
+
   return (
     <div className="flex justify-center">
-      <table className="w-2/3 border">
-        <thead className="text-xs text-gray-700 bg-gray-200 dark:text-gray-700">
-          <tr>
-            <th scope="col" className="px-6 py-5">
-              SIZE
-            </th>
-            <th scope="col" className="px-6 py-3">
-              COLOR
-            </th>
-            <th scope="col" className="px-6 py-3">
-              PRICE
-            </th>
-            <th scope="col" className="px-6 py-3">
-              PHOTO
-            </th>
-            <th scope="col" className="px-6 py-3">
-              DESCRIPTION
-            </th>
-            <th scope="col" className="px-6 py-3">
-              QUANTITY
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {products.map((product) => {
-            if (product.type === ProductType.CLOTHES) {
-              return <ClothesItem key={product.id} product={product as ClothesProductType} />;
-            }
-            if (product.type === ProductType.DRINK) {
-              return <DrinkItem key={product.id} product={product as DrinkProductType} />;
-            }
-            return <FoodItem key={product.id} product={product as FoodProductType} />;
-          })}
-        </tbody>
-      </table>
+      <div className="w-full max-w-3xl py-16 sm:px-0">
+        <Tab.Group>
+          <Tab.List className="flex space-x-1 rounded-xl bg-blue-900/20 p-1">
+            {Object.keys(categories).map((category) => (
+              <Tab
+                key={category}
+                className={({ selected }) =>
+                  classNames(
+                    'w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-blue-700',
+                    'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2',
+                    selected ? 'bg-white shadow' : 'text-blue-100 hover:bg-white/[0.12] hover:text-white',
+                  )
+                }
+              >
+                {category}
+              </Tab>
+            ))}
+          </Tab.List>
+          <Tab.Panels className="mt-2">
+            {Object.values(categories).map((posts, idx) => (
+              <Tab.Panel
+                key={idx}
+                className={classNames(
+                  'rounded-xl bg-white p-3',
+                  'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2',
+                )}
+              ></Tab.Panel>
+            ))}
+          </Tab.Panels>
+        </Tab.Group>
+      </div>
     </div>
   );
 };
