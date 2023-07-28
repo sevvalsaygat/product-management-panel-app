82fbf3708e71b475a4220156edd993605399fe53
sevvalsaygat <sevvallsstt@gmail.com>
2023-07-28 23:35:43 +0300

    refactor: product page design

---
+++ components/Icons/index.ts
@@ -0,0 +1,6 @@
+import SvgClothes from './Clothes';
+import SvgDrink from './Drink';
+import SvgFood from './Food';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default { SvgClothes, SvgDrink, SvgFood };
--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,6 @@ import ProductModal from './ProductModal';
 import ProductForm from './ProductForm';
 import ProductTable from './ProductTable';
 import Form from './Form';
+import Icons from './Icons';
 
-export { ProductModal, ProductForm, ProductTable, Form };
+export { ProductModal, ProductForm, ProductTable, Form, Icons };
